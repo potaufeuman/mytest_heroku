e67c8f37fa9d48658859b686a9cac5bba5edf969
Kenta Shinozaki <3ks4giving@gmail.com>
2020-03-04 05:43:59 +0000

    tutorial 10ch

--- config/application.rb
+++ config/application.rb
@@ -14,17 +14,17 @@ module Mytest
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+        # 色々な記述があるので、一番下に追記する
+        g.test_framework :rspec,
+                         fixtures: true,
+                         view_specs: false,
+                         helper_specs: false,
+                         routing_specs: false,
+                         controller_specs: true,
+                         request_specs: false
+        g.fixture_replacement :factory_bot, dir: "spec/factories"
+      end
   end
-  
-  config.generators do |g|
-      # 色々な記述があるので、一番下に追記する
-      g.test_framework :rspec,
-                       fixtures: true,
-                       view_specs: false,
-                       helper_specs: false,
-                       routing_specs: false,
-                       controller_specs: true,
-                       request_specs: false
-      g.fixture_replacement :factory_bot, dir: "spec/factories"
-    end
 end
+
