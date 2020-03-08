85493312a6de0563f02d5a0040a4787e5c8c2912
Kenta Shinozaki <3ks4giving@gmail.com>
2020-03-08 04:49:12 +0000

    ログイン周りを整備

---
+++ db/migrate/20200308044605_create_tests.rb
@@ -0,0 +1,11 @@
+class CreateTests < ActiveRecord::Migration[5.1]
+  def change
+    create_table :tests do |t|
+      t.string :subject
+      t.string :title
+      t.references :user, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/tests.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :test do
+    subject { "MyString" }
+    title { "MyString" }
+    user { nil }
+  end
+end
