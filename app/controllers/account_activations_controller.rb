a13e331eb90c0242886b522b4a5eccaf005dc222
Kenta Shinozaki <3ks4giving@gmail.com>
2020-03-30 13:43:11 +0000

    有効化メールのリンク先を修正。文章を修正。

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -2,6 +2,7 @@ class AccountActivationsController < ApplicationController
   def edit
     user = User.find_by(email: params[:email])
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
+    # !user.activated?は、有効済リンクを再使用して攻撃できないようにする
       user.activate
       log_in user
       flash[:success] = "Account activated!"
