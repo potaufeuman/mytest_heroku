5bddb1e24ab70e94b87f200d0c45ae9eb4c879a3
Kenta Shinozaki <3ks4giving@gmail.com>
2020-04-05 06:43:23 +0000

    AnswersControllerから不要箇所削除、一部ユーザーデータを直接登録

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -16,35 +16,11 @@ class AnswersController < ApplicationController
   end
   
   def new
-    # @user = current_user
-    # @test = Test.find(params[:test_id])
-    # @question = @test.questions.find(params[:question_id])
-    # answer_selects(@question)
-    # now_answer(@test)  # 回答すると、次の質問にジャンプする。AnswersHelper参照。
-    # @answers = []
-    # (@ia+1).times do
-    #   @answer = Answer.new
-    #   @answers = @answers.push(@answer)
-    # end
+    
   end
   
   def create
-    # @user = current_user
-    # @test = Test.find(params[:test_id])
-    # @question = @test.questions.find(params[:question_id])
-    # @answer = @question.answers.build(answer_params)
-    # answer_selects(@question)
-    # now_answer(@test)  # 回答すると、次の質問にジャンプする。AnswersHelper参照。
-    # if @answer.save
-    #   if @next_question == nil
-    #     tested_id = @user.id
-    #     redirect_to tested_test_answers_path(tested_id, @test.id)  # 採点ページにジャンプする。
-    #   else
-    #     redirect_to new_test_question_answer_path(@test, @next_question.id)
-    #   end
-    # else
-    #   redirect_to request.referrer || mytests_path
-    # end
+    
   end
   
   def destroy
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,33 +1,15 @@
-User.create!(name:  "Example User",
-             email: "example@fortune.org",
-             password:              "foobar",
-             password_confirmation: "foobar",
+# User.create!(name:  "Example User",
+#             email: "example@fortune.org",
+#             password:              "foobar",
+#             password_confirmation: "foobar",
+#             admin:     false,
+#             activated: true,
+#             activated_at: Time.zone.now)
+
+User.create!(name:  "Sumiko",
+             email: "sumiko724@gmail.com",
+             password:              "omakase724",
+             password_confirmation: "omakase724",
              admin:     false,
              activated: true,
              activated_at: Time.zone.now)
-
-# 5.times do |n|
-#   name  = Faker::Name.name
-#   email = "example-#{n+1}@railstutorial.org"
-#   password = "password"
-#   User.create!(name:  name,
-#               email: email,
-#               password:              password,
-#               password_confirmation: password,
-#               activated: true,
-#               activated_at: Time.zone.now)
-# end
-
-# # https://github.com/faker-ruby/faker/blob/master/doc/books/book.md
-# users = User.order(:created_at).take(5)
-# 10.times do
-#   title = Faker::Book.title
-#   subject = Faker::Book.genre
-#   users.each { |user| user.tests.create!(title: title, subject: subject) }
-# end
-
-# tests = Test.order(:created_at).take(5)
-# 10.times do
-#   Q = Faker::Lorem.question
-#   tests.each { |test| test.questions.create!(Q: Q) }
-# end
