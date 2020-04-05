cf571f53a45c2e925512602c27c76f771cd70a52
Kenta Shinozaki <3ks4giving@gmail.com>
2020-04-05 06:57:40 +0000

    seed修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,9 @@
 #             admin:     false,
 #             activated: true,
 #             activated_at: Time.zone.now)
-
+             
 User.create!(name:  "Sumiko",
-             email: "sumiko724@gmail.com",
+             email: "ssumiko724@gmail.com",
              password:              "omakase724",
              password_confirmation: "omakase724",
              admin:     false,
