43ab07fbffd863e6dd07ecb3cd8d7f5c40a8b245
Kenta Shinozaki <3ks4giving@gmail.com>
2020-04-30 07:32:31 +0000

    ユーザー登録のview、メール修正

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,7 +2,8 @@ class UserMailer < ApplicationMailer
 
   def account_activation(user)
     @user = user
-    mail to: user.email, subject: "Account activation"
+    mail to: user.email, subject: default_i18n_subject(user: user.name)
+    "Account activation"
     # subjectは、メールの件名
   end
 
